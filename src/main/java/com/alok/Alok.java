5ebc592b0a820a94a9b01c29325638a31d0d7f8c
Alok <alokkavilkar22@outlook.com>
2022-11-11 11:55:21 +0530

    java file updated

--- src/main/java/com/alok/Alok.java
+++ src/main/java/com/alok/Alok.java
@@ -5,6 +5,7 @@ public class Alok {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome, Alok in Maven-Jenkin-Docker-Image");
+		System.out.println("This is file under Java main resources.");
 	}
 
 }
