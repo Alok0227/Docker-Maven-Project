30c87179b8bfd76de7e00ab75d050096ef9118e6
Alok <alokkavilkar22@outlook.com>
2022-11-11 11:34:48 +0530

    New Project Added

---
+++ src/main/java/com/alok/Alok.java
@@ -0,0 +1,10 @@
+package com.alok;
+
+public class Alok {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome, Alok in Maven-Jenkin-Docker-Image");
+	}
+
+}
